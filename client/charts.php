baaefc1e8743ad2fe01019c783dd24e0c78c7caf
egov-team5 <egov.team5@egovernments.org>
2017-04-23 07:01:06 +0530

    Update

--- client/charts.php
+++ client/charts.php
@@ -83,7 +83,7 @@
                 <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Charts
+                            Complaints
                         </h1>
                         <ol class="breadcrumb">
                             <li>
